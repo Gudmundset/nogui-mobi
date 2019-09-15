15b126fd9e5f37af3a5c5c4bbd798159a3e64f86
Philip A Larson <dynendal@gmail.com>
2019-09-15 10:17:51 -0700

    Update mobi-ride-stats.py

--- mobi-ride-stats.py
+++ mobi-ride-stats.py
@@ -1,28 +1,47 @@
 from collections import Counter
 
 def scrape_ridedata(data):
+  '''
+  Function: return rides + header data from ridedata file
+  '''
   rides = []
   for no,line in enumerate(data):
     if no == 0:
       header_data = line.split('\t')
     line_list = line.split('\t')
     rides.append(dict(zip(header_data, line_list)))
-  return rides
+  return rides, header_data
 
-def main():
-  filepath = "phil_ridedata"
-  with open(filepath) as f:
-    data = f.readlines()
-
-  rides = scrape_ridedata(data)
-
+def ride_stats(rides, headers):
+  '''
+  Function: Find stats on ride objects
+  '''
   c = Counter()
   for r in rides:
     bike = r['BIKE']
-    print(bike)
     c[bike] += 1
 
-  print('most common bike', c.most_common(10))
+  print('most common bikes:') 
+  for n,b in enumerate(c.most_common(10)):
+    print(f'{n+1}: {b})')
+
+
+def main():
+  '''
+  Xhu Li, do The Thing
+  '''
+  filepath = "phil_ridedata"
+  try:
+    with open(filepath) as f:
+      data = f.readlines()
+  except IOError as e:
+    print(e)
+
+  if data:
+    rides, headers = scrape_ridedata(data)
+    ride_stats(rides, headers)
 
 if __name__ == "__main__":
   main()
+
+
